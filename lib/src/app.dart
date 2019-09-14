175df727ef1ca4199596ae086caf3272ed62e575
Neeraj-Palliyali <55308416+Neeraj-Palliyali@users.noreply.github.com>
2019-09-14 11:13:04 +0530

    Initial commit

---
+++ lib/src/app.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class App extends StatefulWidget {
+  createState() {
+    return AppState();
+  }
+}
+
+class AppState extends State<App> {
+  int counter = 0;
+
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter  Library'),
+          drawer : Drawer
+        ),
+      ),
+    );
+  }
+}
